5a41508c90fe0c31abcb3e742f3dde6d0f833b38
firstonline <fffirstonline@gmail.com>
2019-10-08 20:01:23 +0100

    challenge project

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+	[SerializeField] private float m_movementsSpeed = 5f;
+	[SerializeField] private float m_maxWalkingDistance = 10f;
+
+	private Vector3 m_startingPosition;
+	private float m_currentWalkingDistance;
+
+	private void Awake()
+	{
+		m_startingPosition = transform.position;
+	}
+
+	private void Update()
+	{
+		if (m_currentWalkingDistance >= m_maxWalkingDistance)
+		{
+			m_currentWalkingDistance = 0f;
+			transform.position = m_startingPosition;
+		}
+
+		// just move forward
+		var movementVector = transform.forward * m_movementsSpeed * Time.deltaTime;
+		transform.position += movementVector;
+		m_currentWalkingDistance += movementVector.magnitude;
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,59 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	[Header("Spawn Info")]
+	public bool m_spawn = true;
+	public bool m_useECS = false;
+	public GameObject m_prefab;
+
+	[Header("Spawn Timing")]
+	[Range(1, 100)] public int m_spawnsPerInterval = 1;
+	[Range(.1f, 2f)] public float m_spawnInterval = 1f;
+	
+	EntityManager m_manager;
+	Entity m_entityPrefab;
+
+	float m_cooldown;
+
+
+	void Start()
+	{
+		if (m_useECS)
+		{
+			m_manager = World.Active.EntityManager;
+			m_entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(m_prefab, World.Active);
+		}
+	}
+
+	void Update()
+    {
+		m_cooldown -= Time.deltaTime;
+
+		if (m_cooldown <= 0f)
+		{
+			m_cooldown += m_spawnInterval;
+			Spawn();
+		}
+    }
+
+	void Spawn()
+	{
+		for (int i = 0; i < m_spawnsPerInterval; i++)
+		{
+			Vector3 pos = transform.position;
+
+			if (!m_useECS)
+			{
+				Instantiate(m_prefab, pos, Quaternion.identity);
+			}
+			else
+			{
+				Entity enemy = m_manager.Instantiate(m_entityPrefab);
+				m_manager.SetComponentData(enemy, new Translation { Value = pos });
+			}
+		}
+	}
+}
